f4600c71831342c403329bdb465057cf8632bfe8
JE2606 <juan.elias0626@gmail.com>
2024-10-17 16:31:55 -0400

    subida del proyecto

---
+++ lib/mongoose.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URI) {
+        console.log('MONGODB_URI is not defined');
+        return;
+    }
+
+    if (isConnected) {
+        console.log('=> using existing database connection');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        isConnected = true;
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+export default mongoose;
